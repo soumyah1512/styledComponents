515c9a8652f5324c4e38a80c7fdc414147804ba8
soumyah1512 <99666938+soumyah1512@users.noreply.github.com>
2022-05-24 23:42:39 +0530

    chakra ui trial

--- app/src/index.js
+++ app/src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import {Provider} from "react-redux"
 import { ReactRedApp } from './ReactRedux/ReactRedApp';
 import { store } from './ReactRedux/Redux/store';
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ReactRedApp />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <Provider store={store}>
+  //     <ReactRedApp />
+  //   </Provider>
+  // </React.StrictMode>
+  <ChakraProvider>
+    <h1>Abc</h1>
+    <Box as='button' bg="tomato" p="8" w="30%">This is a box</Box>
+  </ChakraProvider>
 );
 
 // 1. Provider
